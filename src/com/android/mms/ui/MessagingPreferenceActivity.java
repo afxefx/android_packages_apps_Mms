1b062031c968b7589fa6dee94f71bc2ed3e0e985
DvTonder <david.vantonder@gmail.com>
2012-11-21 20:08:29 -0500

    Add SMS Split

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -116,6 +116,9 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static final String QM_CLOSE_ALL_ENABLED      = "pref_key_close_all";
     public static final String QM_DARK_THEME_ENABLED     = "pref_dark_theme";
 
+	// Split sms
+    public static final String SMS_SPLIT_COUNTER        = "pref_key_sms_split_counter";
+
     // Menu entries
     private static final int MENU_RESTORE_DEFAULTS    = 1;
 
